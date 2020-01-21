ebf9df3aa4cdb5b9c86a67e2702b4a61ddcbf454
Mikaelpa <palmroosmikael@gmail.com>
2020-01-21 15:09:01 +0200

    Separate front and backend

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('build'))
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World backend</h1>')
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
