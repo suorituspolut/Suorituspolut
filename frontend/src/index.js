1412e587625be981f9cc4465bcb448bd490aa4cc
Jetro Hulkko <jetro.hulkko@helsinki.fi>
2020-01-26 19:22:58 +0200

    D3 added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom'
 import * as d3 from "d3"
 
 d3.selectAll("div").style('color','red')
-.append("svg")
-.attr("width", 900)
-.attr("height", 600)
-.style("border", "5px solid black")
 
 const App = () => (
   <div>
