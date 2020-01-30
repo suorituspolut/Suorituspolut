a1fb49df33fe05ce6c5644c111bc5599b35e2eec
Jetro Hulkko <jetro.hulkko@helsinki.fi>
2020-01-30 18:06:56 +0200

    course component first prototype

--- client/components/App.js
+++ client/components/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import NavBar from 'Components/NavBar'
 import Footer from 'Components/Footer'
 import Router from 'Components/Router'
+import Course from 'Components/Course'
 
 export default () => (
   <div>
+    <Course name = "tira"/>
+    <Course name = "ohpe"/>
     <NavBar />
     <Router />
     <Footer />
