6c221487607469aad9126c4537c08d88bbf8ecc7
Jantunen <hannajant@gmail.com>
2020-02-07 13:26:46 +0200

    data insert test

---
+++ client/components/Datahandler.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts'
+require("highcharts/modules/sankey")(Highcharts)
+require("highcharts/modules/exporting")(Highcharts)
+
