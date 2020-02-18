f3e928dad3786a7e290e03c19cc79cafe679ca89
Mikaelpa <palmroosmikael@gmail.com>
2020-02-18 13:45:03 +0200

    change data path

--- server/controllers/csvController.js
+++ server/controllers/csvController.js
@@ -7,7 +7,8 @@ const fs = require('fs')
 const getAll = async (req, res) => {
 
   // Getting the correct file from backend. 
-  const file = (process.cwd() + '/client/data/anon_dataset.csv')
+  //const file = (process.cwd() + '/client/data/anon_dataset.csv')
+  const file = ('@root/client/data/anon_dataset.csv')
   const array = []
 
   // Parsing the data by semi-colon and creating an object of each line
