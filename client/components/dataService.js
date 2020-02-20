503a02348c1e2c5b3c140cdbc4847cbdd59e22ef
Saara Satokangas <saarasat@users.noreply.guthub.com>
2020-02-20 12:00:43 +0200

    Added course-filter

--- client/components/dataService.js
+++ client/components/dataService.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react'
 import Highcharts from 'highcharts'
-import { Table } from 'semantic-ui-react'
-import { createOptions } from '../util/units'
+import { createNumberOptions, createTextOptions } from '../util/units'
 import Graph from './Graph'
 import Filter from './Filter'
 
 require("highcharts/modules/sankey")(Highcharts)
 require("highcharts/modules/exporting")(Highcharts)
 
-const httpGet = (type) => {
+const getGraphData = (type) => {
   var xmlHttp = new XMLHttpRequest()
   xmlHttp.open( "GET", `http://localhost:8000/api/data/${type}`, false ) 
+  xmlHttp.send( null )
+  return xmlHttp.responseText
+}
+
+const getCourseData = () => {
+  var xmlHttp = new XMLHttpRequest()
+  xmlHttp.open( "GET", `http://localhost:8000/api/courses`, false ) 
   xmlHttp.send( null )
   return xmlHttp.responseText
 }
@@ -19,10 +25,10 @@ const Data = () => { //Getting the data from backend
   const [graphToShow, setGraphToShow] = useState(true)
   const [selectedCourse, setSelectedCourse] = useState("Ohjelmoinnin perusteet")
   const [selectedYear, setSelectedYear] = useState(2017)
-  console.log(selectedYear)
   
-  let normalPaths = JSON.parse(httpGet("E2E"))
-  let e2ePaths = JSON.parse(httpGet("normal"))
+  const normalPaths = JSON.parse(getGraphData("E2E"))
+  const e2ePaths = JSON.parse(getGraphData("normal"))
+  const courses = JSON.parse(getCourseData())
 
   const handleYearChange = (event) => {
     setSelectedYear(event.target.value)
@@ -37,8 +43,10 @@ const Data = () => { //Getting the data from backend
     <div>
       <table className="ui celled table">
         <tbody>
-          <Filter label="Year" handleChange={handleYearChange} options={createOptions(2013, 2020).reverse()} placeholder="2019" />
-          <Filter label="Starting course" handleChange={handleCourseChange} options={["course 1"]} placeholder="Starting course"/>
+          <trow>
+            <Filter label="Year" handleChange={handleYearChange} options={createNumberOptions(2013, 2020).reverse()} placeholder="2019" />
+            <Filter label="Starting course" handleChange={handleCourseChange} options={createTextOptions(courses)} placeholder="Starting course"/>
+          </trow>
         </tbody>
       </table>
 
--- client/util/units.js
+++ client/util/units.js
@@ -1,10 +1,13 @@
 
-export const createOptions = (start, end) => {
+export const createNumberOptions = (start, end) => {
   const array = new Array(end-start)
   for (let i = 0; i < array.length; i++) {
     array[i] = {key: start, value: start, text: start}
     start++
   }
-  console.log(array)
   return array
+}
+
+export const createTextOptions = (array) => {
+  return array.map(item => ({key: item, value: item, text: item}))
 }
