f80e46b133e31fe4c2456245323f65b70baf712d
Mikaelpa <palmroosmikael@gmail.com>
2020-02-25 13:55:59 +0200

    add production fix to get requests

--- client/components/dataService.js
+++ client/components/dataService.js
@@ -19,7 +19,12 @@ const getGraphData = (type, year, course, grade) => {
   if (type === 'normal' || type === 'E2E') {
     urlEnd = `${type}/${year}/${course}/${grade}`
   }
-
+  if (process.env.NODE_ENV !== 'production') {
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open( "GET", `http://localhost:8000/api/data/${urlEnd}`, false ) 
+    xmlHttp.send( null )
+    return xmlHttp.responseText
+  }
   var xmlHttp = new XMLHttpRequest()
   xmlHttp.open( "GET", `https://toska.cs.helsinki.fi/suorituspolut/api/data/${urlEnd}`, false ) 
   xmlHttp.send( null )
@@ -27,6 +32,12 @@ const getGraphData = (type, year, course, grade) => {
 }
 
 const getCourseData = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open( "GET", `http://localhost:8000/api/courses`, false ) 
+    xmlHttp.send( null )
+    return xmlHttp.responseText
+  }
   var xmlHttp = new XMLHttpRequest()
   xmlHttp.open( "GET", `https://toska.cs.helsinki.fi/suorituspolut/api/courses`, false ) 
   xmlHttp.send( null )
