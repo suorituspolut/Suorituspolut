714d5c22c397a99a69860991ce27111290b17a7a
Saara Satokangas <saarasat@users.noreply.guthub.com>
2020-03-17 09:04:01 +0200

    Fixed navbar merge-conflict

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,20 +6,12 @@ const NavBar = ({ onChange, value }) => {
   const options = [
     { key: 1, value: 1, text: 'Sankey (kurssien suorituspolut periodeittain)' },
     { key: 2, value: 2, text: 'Sankey (kurssien suorituspolut periodeittain, toinen periodi)' },
-<<<<<<< HEAD
     { key: 3, value: 3, text: 'Sankey (kurssien suorituspolut periodeittain, aloituskurssit)' },
     { key: 4, value: 4, text: 'Histogrammi (kurssin suoritusajankohta)' },
     { key: 5, value: 5, text: 'Histogrammi (useamman kurssin suoritusajankohta)' },
     { key: 6, value: 6, text: 'Kuplagraafi (listaus kursseista periodeittain)' },
     { key: 7, value: 7, text: 'Pullonkaulakurssit (hahmotelma)' },
     { key: 8, value: 8, text: 'Venn-diagrammi (hahmotelma)' },
-=======
-    { key: 6, value: 6, text: 'Sankey (kurssien suorituspolut periodeittain, aloituskurssit)' },
-    { key: 3, value: 3, text: 'Histogrammi (kurssin suoritusajankohta)' },
-    { key: 7, value: 7, text: 'Kuplagraafi (listaus kursseista periodeittain)' },
-    { key: 4, value: 4, text: 'Pullonkaulakurssit (hahmotelma)' },
-    { key: 5, value: 5, text: 'Venn-diagrammi (hahmotelma)' },
->>>>>>> e88d6eb84097e847c28b5a8ab88f891a650992de
   ]
 
   return (
