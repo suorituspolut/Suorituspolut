8d122634cb5ef13fb937d079944fe131b7042b2e
Jantunen <hannajant@gmail.com>
2020-04-10 11:29:19 +0300

    fix histogram years to show

--- server/datahandling/histogramDataHandler.js
+++ server/datahandling/histogramDataHandler.js
@@ -4,7 +4,7 @@ const { studentObjects } = require('@root/server/datahandling/students')
 
 const courseHistoArray = (students, course) => {
   let sum = 0
-  const histogramArray = new Array(36)
+  const histogramArray = new Array(45)
   for (let i = 0; i < histogramArray.length; i++) {
     histogramArray[i] = 0
   }
@@ -19,7 +19,7 @@ const courseHistoArray = (students, course) => {
       student.courses.forEach((credit) => {
         if (firstCourse.course !== course && credit.course === course) {
           const time = periodsBetweenTwoDates(firstCourse.date, credit.date)
-          if (time < 36) {
+          if (time < 45) {
             histogramArray[time]++
             sum++
           }  
